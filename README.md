ca78c01fa769caf370d5092c1ebe8dc03e266621
narukane <karomyfatahillah@gmail.com>
2023-05-25 22:07:45 +0800

    Intial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# BASIC CRUD RESTful API using Node.js,Express.js and MongoDBThis is a basic ToDo list app built using Node.js, Express.js, and MongoDB. It provides a RESTful API for managing tasks.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [API Routes](#api-routes)
+- [Technologies Used](#technologies-used)
+
+## Features
+
+- Create new tasks
+- Update existing tasks
+- Delete tasks
+- Retrieve all tasks
+
+## Installation
+
+1. Clone the repository.
+
+   ```shell
+   git clone https://github.com/your-username/your-repo.git
+   
+2. Install the dependencies.
+    ```shell
+    cd todo-list
+    npm install
+    
+3.  Start the application
+    ```shell
+    npm start
+    
+Usage
+Explain how to use your ToDo list app. Provide any relevant information, such as how to create tasks, mark them as completed, or delete them. You can also include screenshots or examples to enhance understanding.
+
+API Routes
+If your app has a RESTful API, list the available routes and their descriptions here. For example:
+
+1.  GET /tasks: Retrieves all tasks.
+2.  POST /tasks: Creates a new task.
+3.  PUT /tasks/:id: Updates a specific task.
+4.  DELETE /tasks/:id: Deletes a specific task.
+##  Technologies Used
+-   Node.js
+-   Express.js
+-   MongoDB
---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//mongodb server
+mongoose.connect("mongodb://127.0.0.1:27017/todolist", {
+  useNewUrlParser: true,
+});
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+//schema
+const taskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+//api
+app.get("/tasks", (_, res) => {
+  Task.find()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error retrieving tasks");
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((tasks) => {
+      if(!tasks){
+        res.status(404).send("Task not found");
+      }else{
+        res.json(tasks);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error retrieving tasks");
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(200).json({ task: "Task added successfully" });
+    })
+    .catch(() => {
+      res.status(400).send("Adding new task failed");
+    });
+});
+
+app.put("/tasks/:id", (req, res) => {
+  Task.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.status(200).json({ task: "Task updated successfully" });
+    })
+    .catch(() => {
+      res.status(400).send("Updating task failed");
+    });
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  Task.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(200).json({ task: "Task deleted successfully" });
+    })
+    .catch(() => {
+      res.status(400).send("Deleting task failed");
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
